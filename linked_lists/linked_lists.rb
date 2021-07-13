9638f675a629342375d1bdb2c0c20649ece62a21
spookyorange <korkmazcoltu@mail.ru>
2021-07-13 13:12:24 +0300

    append ready to go, prepend in progress

--- linked_lists/linked_lists.rb
+++ linked_lists/linked_lists.rb
@@ -1,35 +1,59 @@
+# frozen_string_literal: true
+
 class Node
   def initialize(value = nil, next_node = nil)
     @value = value
     @next_node = next_node
-    @@ref += 1
-    @ref = @@ref
   end
 
-  def self.ref
-    @@ref
-  end
-  @@ref = 0
-  attr_reader :value, :next_node, :ref
+  attr_reader :value, :next_node
+  attr_accessor :next_node
 end
 
 class LinkedList
   def initialize
-    @length = 1
-    @tail = Node.new(nil, nil)
+    @tail = Node.new
+    @head = Node.new(nil, @tail)
   end
 
   def append(value)
-    @a = Node.new(value, @tail)
+    if @head.value.nil?
+      @head = Node.new(value, @tail)
+    elsif @head.next_node.value.nil?
+      @tail = Node.new(value, nil)
+      @head.next_node = @tail
+    else
+      @new_tail = Node.new(value, nil)
+      @tail.next_node = @new_tail
+      @tail = @new_tail
+    end
   end
   
-  def tri_ref
-    @tail.ref
-    @a.ref
+  def prepend(value)
+    if @head.value.nil?
+      if @tail.value.nil?
+        @head = Node.new(value)
+      else
+        @head = Node.new(value, @tail)
+      end
+    else
+      @new_head = Node.new(value)
+      @new_head.next_node = @head
+      @head = @new_head
+    end
+  end
+
+  def info
+    until @head.nil?
+      puts @head.value
+      @head = @head.next_node
+    end
   end
 end
 
 list = LinkedList.new
-list.append('hah')
-puts list
-puts list.tri_ref
+list.append('haha')
+list.prepend('three')
+list.prepend('two')
+list.prepend('one')
+list.info
